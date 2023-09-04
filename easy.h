21bbf0661129e7f2bba2586b875b7370f28fc1a6
Tatiana <qsef@yandex.ru>
2023-09-04 10:48:52 +0300

    первая сдача

---
+++ easy.h
@@ -0,0 +1,6 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+
+int summ(int a, int b);
+
+#endif // EASY_H_INCLUDED
---
+++ summ.cpp
@@ -0,0 +1,5 @@
+#include "easy.h"
+
+int summ(int a, int b){
+     return a + b;
+}
